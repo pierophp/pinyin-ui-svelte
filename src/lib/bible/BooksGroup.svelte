c18c4c5d51647bd66c47f2f8abc4d26c9f1d1d0e
Piero Giusti <pierophp@gmail.com>
2021-12-15 18:20:44 -0300

    Prettier config

--- src/lib/bible/BooksGroup.svelte
+++ src/lib/bible/BooksGroup.svelte
@@ -1,28 +1,28 @@
 <script type="ts">
-	export let books;
-	import bookName from '$lib/data/names';
+  export let books;
+  import bookName from "$lib/data/names";
 </script>
 
 <div class="flex flex-wrap p-5">
-	{#each books as book}
-		<div class="w-1/5 p-1">
-			<div class="p-1 text-white cursor-pointer hover:opacity-70 text-sm color-{book.color}">
-				<span class="hidden md:block">{bookName(book.name, 'cmn-hant')}</span>
-				<span class="md:hidden">{bookName(book.name, 'cmn-hant-abbr')}</span>
-			</div>
-		</div>
-	{/each}
+  {#each books as book}
+    <div class="w-1/5 p-1">
+      <div class="p-1 text-white cursor-pointer hover:opacity-70 text-sm color-{book.color}">
+        <span class="hidden md:block">{bookName(book.name, "cmn-hant")}</span>
+        <span class="md:hidden">{bookName(book.name, "cmn-hant-abbr")}</span>
+      </div>
+    </div>
+  {/each}
 </div>
 
 <style>
-	.color-1 {
-		@apply bg-violet-500;
-	}
-	.color-2 {
-		@apply bg-violet-600;
-	}
+  .color-1 {
+    @apply bg-violet-500;
+  }
+  .color-2 {
+    @apply bg-violet-600;
+  }
 
-	.color-3 {
-		@apply bg-violet-800;
-	}
+  .color-3 {
+    @apply bg-violet-800;
+  }
 </style>
--- src/routes/bible.svelte
+++ src/routes/bible.svelte
@@ -1,30 +1,30 @@
 <script context="module">
-	import { browser } from '$app/env';
-	import books from '$lib/data/books';
-	import BooksGroup from '$lib/bible/BooksGroup.svelte';
+  import { browser } from "$app/env";
+  import books from "$lib/data/books";
+  import BooksGroup from "$lib/bible/BooksGroup.svelte";
 
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
+  // ...but if the client-side router is already loaded
+  // (i.e. we came here from elsewhere in the app), use it
+  export const router = browser;
 
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
+  // since there's no dynamic data here, we can prerender
+  // it so that it gets served as a static asset in prod
+  export const prerender = true;
 </script>
 
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <div class="content">
-	<BooksGroup books={books.hebrew} />
-	<BooksGroup books={books.greek} />
+  <BooksGroup books={books.hebrew} />
+  <BooksGroup books={books.greek} />
 </div>
 
 <style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
-	}
+  .content {
+    width: 100%;
+    max-width: var(--column-width);
+    margin: var(--column-margin-top) auto 0 auto;
+  }
 </style>
