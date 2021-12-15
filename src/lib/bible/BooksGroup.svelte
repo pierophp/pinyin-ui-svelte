afdeb255c6de94d0fc600b544db6eff9304526d6
Piero Giusti <pierophp@gmail.com>
2021-12-15 10:06:49 -0300

    Book color

--- src/lib/bible/BooksGroup.svelte
+++ src/lib/bible/BooksGroup.svelte
@@ -6,10 +6,23 @@
 <div class="flex flex-wrap p-5">
 	{#each books as book}
 		<div class="w-1/5 p-1">
-			<div class="p-1 bg-violet-600 text-white cursor-pointer hover:opacity-70 text-sm">
+			<div class="p-1 text-white cursor-pointer hover:opacity-70 text-sm color-{book.color}">
 				<span class="hidden md:block">{bookName(book.name, 'cmn-hant')}</span>
 				<span class="md:hidden">{bookName(book.name, 'cmn-hant-abbr')}</span>
 			</div>
 		</div>
 	{/each}
 </div>
+
+<style>
+	.color-1 {
+		@apply bg-violet-500;
+	}
+	.color-2 {
+		@apply bg-violet-600;
+	}
+
+	.color-3 {
+		@apply bg-violet-800;
+	}
+</style>
